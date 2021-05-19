891f346e68b6beec51cdfe1ba1472fc1b96bc053
Ana204 <anagomes.lucia@hotmail.com>
2021-05-18 22:46:21 -0300

    Lesson on functions

--- functions.js
+++ functions.js
@@ -1,1 +1,17 @@
-console.log("Hello Word!!")
+console.log("Lesson on functions")
+
+//Uma das vantagens de usar funções é não ficar com repetição de codigos, caso for necessario apenas chamar a função
+//Function é um tipo de dado estrutural, ou seja uma estrutura de dados
+
+function createPhrases() {
+    console.log("Estudar é muito bom!")
+    console.log("Paciência e persistência!")
+    console.log("Revisão é mãe do aprendizado!")
+}
+
+//Como eu chamo essa função? Apenas chamo a pelo nome dado
+
+//execute, run, call, invoke
+createPhrases() 
+createPhrases() 
+createPhrases() 
